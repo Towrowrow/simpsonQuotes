cac52c651d949effb971e8f77993df7942d10c64
Tilmant Rémi <towtowrow@gmail.com>
2019-03-26 14:59:44 +0100

    GGWP

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import logo2 from "./logo2.svg";
+
+import "./App.css";
+import Quotes from "./Quotes";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working:true
+    };
+  }
+  handleClick = () => {
+    this.setState({ working: !this.state.working });
+  };
+
   render() {
+    const work = this.state.working ? 'App-logo' : 'App-logo-off';
+    const workbutton = this.state.working ? 'Homer is working' : 'Doh !';
+    const logoo = this.state.working ? logo : logo2;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logoo} className={work} alt="logo" />
+          
+          <h1 className="App-title">Simpsons Quotes</h1>
         </header>
-      </div>
+        <div className="Work">
+       <button  onClick={this.handleClick}>{workbutton}</button>
+ 
+        <Quotes/>
+      
+  
+
+
+  
+     </div>
+     </div>
     );
   }
 }
-
 export default App;
